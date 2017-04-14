fa61207aed8a7a908f5bb828420b1bd47bbbafe8
andyyou <andyyu0920@gmail.com>
2017-04-14 17:18:55 +0800

    Unpick App.vue to reservation.vue & setup.vue

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import axios from 'axios'
 import VuePanZoom from '@/plugins/vue-svg-pan-zoom'
-import App from '@/components/App'
 
-Vue.prototype.$http = axios
+axios.defaults.baseURL = 'https://xeats.herokuapp.com/v1.0'
+Vue.prototype.$axios = axios
 
 /**
  * TODO: Those variables need from API
  */
-const WIDTH = 800
-const HEIGHT = 600
+// const WIDTH = 800
+// const HEIGHT = 600
 
-const VIEWPORT = {
-  width: 1913.7,
-  height: 937.3
-}
+// const VIEWPORT = {
+//   width: 1913.7,
+//   height: 937.3
+// }
 
 var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                             window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
@@ -26,36 +26,57 @@ Vue.use(VuePanZoom)
 /**
  * SDK Main Class
  */
+
+
 class Xeat {
   constructor (options) {
-    this.options = options
-    /**
-     * TODO:
-     * Now get data from variables
-     * 
-     *   0. Get viewbox size
-     *   1. Get seats
-     */
-    return new Vue({
-      el: this.options.el,
-      template: `
-        <app 
-          :width="${this.options.width}" 
-          :height="${this.options.height}" 
-          spot="${this.options.spot}"
-          :zoomMax="${this.options.zoomMax}"
-          :zoomMin="${this.options.zoomMin}"
-          :token="${this.options.token}"
-          :autoSize="${this.options.autoSize}"
-          :amountMax="${this.options.amountMax}"
-          :amountMin="${this.options.amountMin}"
-        >
-        </app>
-      `,
-      components: {
-        App
-      }
-    })
+    const componentNames = ['reversation', 'setup']
+
+    if (!options.el && !/([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/g.test(options.el)) {
+      throw new Error('el attribute has no setting')
+    }
+
+    if (options.component && options.component.name && componentNames.indexOf(options.component.name)) {
+      const app = require(`@/components/${options.component.name}`)
+      return new Vue({
+        el: options.el,
+        data: {
+          token: null
+        },
+        created () {
+          this.setToken()
+        },
+        methods: {
+          setToken () {
+            this.$axios.post('/users/token', {
+              access_key: options.accessKey,
+              secret: options.secret
+            }).then(function (res) {
+              localStorage.setItem('_x_t', res.data.token)
+            })
+          }
+        },
+        render (createElement) {
+          return createElement('app', {
+            props: {
+              width: options.width,
+              height: options.height,
+              source: options.component.source,
+              zoomMax: options.zoomMax,
+              zoomMin: options.zoomMin,
+              autoSize: options.autoSize,
+              amountMax: options.amountMax,
+              amountMin: options.amountMin
+            }
+          })
+        },
+        components: {
+          app
+        }
+      })
+    } else {
+      throw new Error('Specific invalid Xeat component type')
+    }
   }
 }
 
