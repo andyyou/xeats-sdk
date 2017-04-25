000c5b9650aecd09e98676e1b5390d0a45ce12e7
andyyou <andyyu0920@gmail.com>
2017-04-25 10:53:14 +0800

    Fix request token issue

--- src/index.js
+++ src/index.js
@@ -12,17 +12,6 @@ window.requestAnimationFrame = requestAnimationFrame
 Vue.use(VuePanZoom)
 Vue.use(VuePicking)
 
-function setToken (accessKey, secret) {
-  axios.post('/users/token', {
-    access_key: accessKey,
-    secret: secret
-  }).then(function (res) {
-    localStorage.setItem('_x_t', res.data.token)
-  }).catch(function (err) {
-    console.log(err)
-  })
-}
-
 /**
  * SDK Main Class
  */
@@ -33,40 +22,44 @@ class Xeat {
     if (!options.el && !/([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/g.test(options.el)) {
       throw new Error('el attribute has no setting')
     }
-    
-    // Initialize token
-    setToken(options.accessKey, options.secret)
 
     if (options.component && options.component.name && componentNames.indexOf(options.component.name) !== -1) {
       const app = require(`@/components/${options.component.name}`)
-      return new Vue({
-        el: options.el,
-        methods: {
-          setToken () {
-            setToken(options.accessKey, options.secret)
-          },
-          getToken () {
-            return localStorage.getItem('_x_t')
-          }
-        },
-        render (createElement) {
-          return createElement('app', {
-            props: {
-              width: options.width,
-              height: options.height,
-              sourceId: options.component.sourceId,
-              zoomMax: options.zoomMax,
-              zoomMin: options.zoomMin,
-              amountMax: options.amountMax,
-              amountMin: options.amountMin,
-              categories: options.component.data.categories
-            }
-          })
-        },
-        components: {
-          app
-        }
+
+      axios.post('/users/token', {
+        access_key: options.accessKey,
+        secret: options.secret
       })
+      .then(function (res) {
+        localStorage.setItem('_x_t', res.data.token)
+        return res.data.token
+      })
+      .then(function (token) {
+        return new Vue({
+          el: options.el,
+          render (createElement) {
+            return createElement('app', {
+              props: {
+                width: options.width,
+                height: options.height,
+                sourceId: options.component.sourceId,
+                zoomMax: options.zoomMax,
+                zoomMin: options.zoomMin,
+                amountMax: options.amountMax,
+                amountMin: options.amountMin,
+                categories: options.component.data.categories
+              }
+            })
+          },
+          components: {
+            app
+          }
+        })
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+      
     } else {
       throw new Error('Specific invalid Xeat component type')
     }
