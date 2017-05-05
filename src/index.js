c7830f68363f6aa1413f589346757b21b876eb48
pjchender <pjchender@gmail.com>
2017-05-05 17:34:40 +0800

    Reset to sourceId for seatsId and spotsId

--- src/index.js
+++ src/index.js
@@ -43,7 +43,6 @@ class Xeat {
                 width: options.width,
                 height: options.height,
                 sourceId: options.component.sourceId,
-                seatsId: options.component.seatsId,
                 zoomMax: options.zoomMax,
                 zoomMin: options.zoomMin,
                 amountMax: options.amountMax,
